3176934bf55c78675632584e22188f21fecf332e
Tyler Wong <50033387+CodeBreakTW@users.noreply.github.com>
2019-09-14 14:44:03 -0400

    Added front-end files, unfinished

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function(){
+    $("input.help-button").click(function(){
+
+      if($("#help-content").height() > 0){
+        $("#help-content").css("height","0px");
+        $("input.help-button").css("padding-bottom","20px");
+
+        $("input.help-button").val("+");
+
+
+      } else{
+        $("#help-content").css("height","100px");
+        $("input.help-button").css("padding-bottom","120px");
+        $("input.help-button").val("-");
+      }
+
+    });
+  });
